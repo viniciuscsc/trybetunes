c75ce8732203fba903053d2b4ebfcc22513fcb26
cunhavcs <cunha.vcs@gmail.com>
2023-01-14 16:49:30 -0300

    Requisito 01: cria as rotas necessárias para a aplicação

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile">
+        Profile
+      </div>
+    );
+  }
+}
+
+export default Profile;
