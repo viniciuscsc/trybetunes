2d46609ecbefbd54f0a545553576a4c912f0b877
cunhavcs <cunha.vcs@gmail.com>
2023-01-16 14:50:10 -0300

    Requisito 08: cria o mecanismo para adicionar músicas na lista de favoritas

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from '../components/Carregando';
 
 class Album extends Component {
   state = {
-    carregando: false,
+    carregando: true,
     musicas: [],
+    musicasFavoritas: [],
   };
 
   async componentDidMount() {
     await this.buscaMusicasDoAlbum();
+    const musicasFavoritas = await getFavoriteSongs();
+    this.setState({ carregando: false, musicasFavoritas });
   }
 
   buscaMusicasDoAlbum = async () => {
@@ -22,25 +27,28 @@ class Album extends Component {
   };
 
   render() {
-    const { musicas } = this.state;
+    const { carregando, musicas, musicasFavoritas } = this.state;
     const listaDeMusicas = musicas.filter((_musica, index) => index !== 0);
 
     return (
       <div data-testid="page-album">
         <Header />
-        {(musicas.length > 0)
-          && (
-            <div>
-              <h3 data-testid="artist-name">{musicas[0].artistName}</h3>
-              <h3 data-testid="album-name">{musicas[0].collectionName}</h3>
-              {listaDeMusicas
-                .map((musica) => (
-                  <MusicCard
-                    key={ musica.trackId }
-                    musica={ musica }
-                  />))}
-            </div>
-          )}
+        {(carregando)
+          ? <Carregando />
+          : (musicas.length > 0)
+              && (
+                <div>
+                  <h3 data-testid="artist-name">{musicas[0].artistName}</h3>
+                  <h3 data-testid="album-name">{musicas[0].collectionName}</h3>
+                  {listaDeMusicas
+                    .map((musica) => (
+                      <MusicCard
+                        key={ musica.trackId }
+                        musica={ musica }
+                        musicasFavoritas={ musicasFavoritas }
+                      />))}
+                </div>
+              )}
       </div>
     );
   }
