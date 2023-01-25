e34270d9b3a76ec2cc3e91a570ac095eb6ac63be
cunhavcs <cunha.vcs@gmail.com>
2023-01-25 12:19:53 -0300

    Requisito 13 concluído

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,50 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Carregando from '../components/Carregando';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  state = {
+    carregando: false,
+    usuario: {},
+  };
+
+  componentDidMount() {
+    this.recuperaInfoUsuario();
+  }
+
+  recuperaInfoUsuario = async () => {
+    const usuario = await getUser();
+    this.setState({ usuario });
+  };
+
+  renderizaDadosUsuario = () => {
+    const { usuario: { description, email, image, name } } = this.state;
+    return (
+      <div>
+        { 'Nome: ' }
+        <p>{ name }</p>
+        { 'Email: ' }
+        <p>{ email }</p>
+        { 'Descrição: ' }
+        <p>{ description }</p>
+        <img data-testid="profile-image" src={ image } alt={ `Imagem de ${name}` } />
+        <br />
+        <br />
+        <Link to="/profile/edit">Editar perfil</Link>
+      </div>
+    );
+  };
+
   render() {
+    const { carregando } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {(carregando)
+          ? <Carregando />
+          : this.renderizaDadosUsuario()}
       </div>
     );
   }
